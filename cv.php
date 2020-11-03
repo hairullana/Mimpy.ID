71699121455362eca8e14192e310af2d145ea6cc
Hairul Lana <hairullana99@gmail.com>
2020-11-03 09:09:48 +0800

    create cv page

---
+++ cv.php
@@ -0,0 +1,119 @@
+<!-- header -->
+<?php 
+$title = "CV Hairul Lana";
+include '_header.php' ?>
+
+
+
+<!-- form -->
+<div class="container my-5">
+    <div class="row">
+        <div class="col">
+            <div class="card">
+                <div class="card-header text-center">
+                    <h3 class="display-4">Curriculum Vitae</h3>
+                </div>
+                <div class="card-body">
+                    <div class="row">
+
+                        <!-- kolom 1 -->
+                        <div class="card col-md-6 py-3">
+                            <!-- image -->
+                            <div class="row my-3">
+                                <div class="col text-center">
+                                    <img src="assets/profile.jpg" alt="" class="rounded img-fluid" width=35% style="max-width:200px">
+                                </div>
+                            </div>
+                            <!-- end image -->
+
+
+
+                            <!-- biodata -->
+                            <div class="row mt-5">
+                                <div class="col-md-12 mb-3">
+                                    <h3>Biodata</h3>
+                                    <table class="table mt-4">
+                                        <tr>
+                                            <td>Nama</td>
+                                            <td>:</td>
+                                            <td>Hairul Lana</td>
+                                        </tr>
+                                        <tr>
+                                            <td>Tanggal Lahir</td>
+                                            <td>:</td>
+                                            <td>27 Oktober 1999</td>
+                                        </tr>
+                                        <tr>
+                                            <td>Jenis Kelamin</td>
+                                            <td>:</td>
+                                            <td>Laki-laki</td>
+                                        </tr>
+                                        <tr>
+                                            <td>Email</td>
+                                            <td>:</td>
+                                            <td>hairullana99@gmail.com</td>
+                                        </tr>
+                                        <tr>
+                                            <td>Nomor Telp</td>
+                                            <td>:</td>
+                                            <td>082123456789</td>
+                                        </tr>
+                                        <tr>
+                                            <td>Alamat</td>
+                                            <td>:</td>
+                                            <td>Jl. Raya Sengkidu, Manggis, Karangasem, Bali</td>
+                                        </tr>
+                                    </table>
+                                </div>
+                            </div>
+                            <!-- end biodata -->
+                        </div>
+                    
+
+                        <!-- kolom 2 -->
+                        <div class="card col-md-6 py-3">
+                            <div class="row ml-1">
+                                <div class="col-md-12">
+                                    <h3>Riwayat Pendidikan</h3>
+                                    <br>2006 - 2012 : SD N 1 Jimbaran
+                                    <br>2012 - 2015 : SMP N 1 Jimbaran
+                                    <br>2015 - 2018 : SMA N 1 Jimbaran (IPA)
+                                </div>
+                            </div>
+                            <div class="row mt-4 ml-1">
+                                <div class="col-md-12">
+                                    <h3>Riwayat Prestasi</h3>
+                                    <br>2006 : Juara 1 Makan Krupuk
+                                    <br>2012 : Juara 1 Lomba Memancing Amarah
+                                    <br>2015 : Juara 2 Lomba Debat Nasional
+                                </div>
+                            </div>
+                            <div class="row mt-4 ml-1">
+                                <div class="col-md-12">
+                                    <h3>Riwayat Pekerjaan</h3>
+                                    <br>2018 - 2019 : Customer Service di PT. Anjay
+                                    <br>2019 - 2020 : Mekanik di Bengkel Anjay
+                                </div>
+                            </div>
+                            <div class="row mt-4 ml-1">
+                                <div class="col-md-12">
+                                    <h3>Keahlian dan Hobi</h3>
+                                    <br>Otomotif
+                                    <br>Web Programming
+                                    <br>Rebahan Bagai di Surga
+                                </div>
+                            </div>
+                        </div>
+
+                    
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+</div>
+<!-- end form -->
+
+<!-- footer -->
+<?php include '_footer.php' ?>
