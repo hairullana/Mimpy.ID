4ad0b0609e4e55fa4fcd7e983307c6ac8cba222f
Hairul Lana <hairullana99@gmail.com>
2020-12-11 16:19:28 +0800

    add backend to edit-loker.php

--- buat-loker.php
+++ buat-loker.php
@@ -38,7 +38,7 @@ session_start();
                     <div class="form-group">
                         <select class="form-control" id="exampleFormControlSelect1" name="lulusan">
                             <option selected disabled>Lulusan</option>
-                            <option value="tanpa ijasah">Tanpa Ijasah</option>
+                            <option value="Tanpa Ijasah">Tanpa Ijasah</option>
                             <option value="SD">SD</option>
                             <option value="SMP">SMP</option>
                             <option value="SMA">SMA/K</option>
--- edit-loker.php
+++ edit-loker.php
@@ -12,11 +12,6 @@ $id = $_GET["id"];
 // ambil data loker
 $loker = mysqli_query($db, "SELECT * from loker where id = $id");
 $loker = mysqli_fetch_assoc($loker);
-// ambil id perusahaan
-$idPerusahaan = $loker["idPerusahaan"];
-// ambil data perusahaan 
-$perusahaan = mysqli_query($db, "SELECT * from perusahaan where id = $idPerusahaan");
-$perusahaan = mysqli_fetch_assoc($perusahaan);
 
 ?>
 
@@ -37,9 +32,162 @@ $perusahaan = mysqli_fetch_assoc($perusahaan);
 
     <div class="row mt-5">
         <div class="col-md-6 offset-md-3">
-            <div class="card">
+            <div class="card shadow-lg">
                 <div class="card-header text-center">
                     <h3>Edit Data Loker</h3>
+                </div>
+                <div class="card-body">
+                    <form action="" method="POST">
+                        <div class="form-group">
+                            <input type="text" class="form-control" placeholder="Posisi" name="posisi" value="<?= $loker["posisi"] ?>">
+                        </div>
+                        <div class="form-group">
+                            <select class="form-control" id="exampleFormControlSelect1" name="lulusan">
+                                <?php if ($loker["lulusan"] == "Tanpa Ijasah") : ?>
+                                    <option selected value="Tanpa Ijasah">Tanpa Ijasah</option>
+                                    <option value="SD">SD</option>
+                                    <option value="SMP">SMP</option>
+                                    <option value="SMA">SMA/K</option>
+                                    <option value="D1">D1</option>
+                                    <option value="D2">D2</option>
+                                    <option value="D3">D3</option>
+                                    <option value="D4">D4</option>
+                                    <option value="S1">S1</option>
+                                    <option value="S2">S2</option>
+                                    <option value="S3">S3</option>
+                                <?php elseif ($loker["lulusan"] == "SD") : ?>
+                                    <option value="Tanpa Ijasah">Tanpa Ijasah</option>
+                                    <option selected value="SD">SD</option>
+                                    <option value="SMP">SMP</option>
+                                    <option value="SMA">SMA/K</option>
+                                    <option value="D1">D1</option>
+                                    <option value="D2">D2</option>
+                                    <option value="D3">D3</option>
+                                    <option value="D4">D4</option>
+                                    <option value="S1">S1</option>
+                                    <option value="S2">S2</option>
+                                    <option value="S3">S3</option>
+                                <?php elseif ($loker["lulusan"] == "SMP") : ?>
+                                    <option value="Tanpa Ijasah">Tanpa Ijasah</option>
+                                    <option value="SD">SD</option>
+                                    <option selected value="SMP">SMP</option>
+                                    <option value="SMA">SMA/K</option>
+                                    <option value="D1">D1</option>
+                                    <option value="D2">D2</option>
+                                    <option value="D3">D3</option>
+                                    <option value="D4">D4</option>
+                                    <option value="S1">S1</option>
+                                    <option value="S2">S2</option>
+                                    <option value="S3">S3</option>
+                                <?php elseif ($loker["lulusan"] == "SMA") : ?>
+                                    <option value="Tanpa Ijasah">Tanpa Ijasah</option>
+                                    <option value="SD">SD</option>
+                                    <option value="SMP">SMP</option>
+                                    <option selected value="SMA">SMA/K</option>
+                                    <option value="D1">D1</option>
+                                    <option value="D2">D2</option>
+                                    <option value="D3">D3</option>
+                                    <option value="D4">D4</option>
+                                    <option value="S1">S1</option>
+                                    <option value="S2">S2</option>
+                                    <option value="S3">S3</option>
+                                <?php elseif ($loker["lulusan"] == "D1") : ?>
+                                    <option value="Tanpa Ijasah">Tanpa Ijasah</option>
+                                    <option value="SD">SD</option>
+                                    <option value="SMP">SMP</option>
+                                    <option value="SMA">SMA/K</option>
+                                    <option selected value="D1">D1</option>
+                                    <option value="D2">D2</option>
+                                    <option value="D3">D3</option>
+                                    <option value="D4">D4</option>
+                                    <option value="S1">S1</option>
+                                    <option value="S2">S2</option>
+                                    <option value="S3">S3</option>
+                                <?php elseif ($loker["lulusan"] == "D2") : ?>
+                                    <option value="Tanpa Ijasah">Tanpa Ijasah</option>
+                                    <option value="SD">SD</option>
+                                    <option value="SMP">SMP</option>
+                                    <option value="SMA">SMA/K</option>
+                                    <option value="D1">D1</option>
+                                    <option selected value="D2">D2</option>
+                                    <option value="D3">D3</option>
+                                    <option value="D4">D4</option>
+                                    <option value="S1">S1</option>
+                                    <option value="S2">S2</option>
+                                    <option value="S3">S3</option>
+                                <?php elseif ($loker["lulusan"] == "D3") : ?>
+                                    <option value="Tanpa Ijasah">Tanpa Ijasah</option>
+                                    <option value="SD">SD</option>
+                                    <option value="SMP">SMP</option>
+                                    <option value="SMA">SMA/K</option>
+                                    <option value="D1">D1</option>
+                                    <option value="D2">D2</option>
+                                    <option selected value="D3">D3</option>
+                                    <option value="D4">D4</option>
+                                    <option value="S1">S1</option>
+                                    <option value="S2">S2</option>
+                                    <option value="S3">S3</option>
+                                <?php elseif ($loker["lulusan"] == "D4") : ?>
+                                    <option value="Tanpa Ijasah">Tanpa Ijasah</option>
+                                    <option value="SD">SD</option>
+                                    <option value="SMP">SMP</option>
+                                    <option value="SMA">SMA/K</option>
+                                    <option value="D1">D1</option>
+                                    <option value="D2">D2</option>
+                                    <option value="D3">D3</option>
+                                    <option selected value="D4">D4</option>
+                                    <option value="S1">S1</option>
+                                    <option value="S2">S2</option>
+                                    <option value="S3">S3</option>
+                                <?php elseif ($loker["lulusan"] == "S1") : ?>
+                                    <option value="Tanpa Ijasah">Tanpa Ijasah</option>
+                                    <option value="SD">SD</option>
+                                    <option value="SMP">SMP</option>
+                                    <option value="SMA">SMA/K</option>
+                                    <option value="D1">D1</option>
+                                    <option value="D2">D2</option>
+                                    <option value="D3">D3</option>
+                                    <option value="D4">D4</option>
+                                    <option selected value="S1">S1</option>
+                                    <option value="S2">S2</option>
+                                    <option value="S3">S3</option>
+                                <?php elseif ($loker["lulusan"] == "S2") : ?>
+                                    <option value="Tanpa Ijasah">Tanpa Ijasah</option>
+                                    <option value="SD">SD</option>
+                                    <option value="SMP">SMP</option>
+                                    <option value="SMA">SMA/K</option>
+                                    <option value="D1">D1</option>
+                                    <option value="D2">D2</option>
+                                    <option value="D3">D3</option>
+                                    <option value="D4">D4</option>
+                                    <option value="S1">S1</option>
+                                    <option selected value="S2">S2</option>
+                                    <option value="S3">S3</option>
+                                <?php elseif ($loker["lulusan"] == "S3") : ?>
+                                    <option value="Tanpa Ijasah">Tanpa Ijasah</option>
+                                    <option value="SD">SD</option>
+                                    <option value="SMP">SMP</option>
+                                    <option value="SMA">SMA/K</option>
+                                    <option value="D1">D1</option>
+                                    <option value="D2">D2</option>
+                                    <option value="D3">D3</option>
+                                    <option value="D4">D4</option>
+                                    <option value="S1">S1</option>
+                                    <option value="S2">S2</option>
+                                    <option selected value="S3">S3</option>
+                                <?php endif; ?>
+                            </select>
+                        </div>
+                        <div class="form-group">
+                            <textarea class="form-control" name="jobdesk" id="" placeholder="Jobdesk" name="jobdesk"><?= $loker["jobdesk"] ?></textarea>
+                        </div>
+                        <div class="form-group">
+                            <textarea class="form-control" name="keterangan" id="" placeholder="Keterangan" name="keterangan"><?= $loker["keterangan"] ?></textarea>
+                        </div>
+                        <div class="form-group text-center">
+                            <button class="btn btn-primary" name="simpanPerubahan">Simpan Perubahan</button>
+                        </div>
+                    </form>
                 </div>
             </div>
         </div>
@@ -54,23 +202,12 @@ $perusahaan = mysqli_fetch_assoc($perusahaan);
 
 <?php
 
-// jika yg login bukan yg punya perusahaan, tendang ke index
-if ($perusahaan["email"] != $_SESSION["perusahaan"]){
-    echo "
-        <script>
-            Swal.fire('AKSES DITOLAK','Anda Tidak Berhak Ke Halaman Ini','warning').then(function(){
-                window.location = 'index.php';
-            });
-        </script>
-    ";
-}
-
 // jika belum login perusahaan, tendang ke index
 if (!isset($_SESSION["perusahaan"])){
     echo "
         <script>
             Swal.fire('AKSES DITOLAK','','success').then(function(){
-                window.location = 'data-loker.php';
+                window.location = 'index.php';
             });
         </script>
     ";
@@ -80,16 +217,11 @@ if (!isset($_SESSION["perusahaan"])){
         echo "
             <script>
                 Swal.fire('AKSI DITOLAK','Tidak Dapat Mengakses Halaman','error').then(function(){
-                    window.location = 'data-loker-perusahaan.php';
+                    window.location = 'data-loker.php';
                 });
             </script>
         ";
     }else {     // jika menerima inputan get id
-        // ambil data id 
-        $id = $_GET["id"];
-        // ambil data loker
-        $loker = mysqli_query($db, "SELECT * from loker where id = $id");
-        $loker = mysqli_fetch_assoc($loker);
         // ambil data perusahaan
         $idPerusahaan = $loker["idPerusahaan"];
         $perusahaan = mysqli_query($db, "SELECT * from perusahaan where id = $idPerusahaan");
@@ -100,7 +232,7 @@ if (!isset($_SESSION["perusahaan"])){
             echo "
                 <script>
                     Swal.fire('AKSES DITOLAK','Loker Ini Bukan Milik Perusahaan Anda','success').then(function(){
-                        window.location = 'data-loker-perusahaan.php';
+                        window.location = 'data-loker.php';
                     });
                 </script>
             ";
@@ -108,5 +240,31 @@ if (!isset($_SESSION["perusahaan"])){
     }
 }
 
+
+// jika tombol Simpan Perubahan di klik
+if (isset($_POST["simpanPerubahan"])){
+    // ambil value form
+    $posisi = htmlspecialchars($_POST["posisi"]);
+    $lulusan = htmlspecialchars($_POST["lulusan"]);
+    $jobdesk = htmlspecialchars($_POST["jobdesk"]);
+    $keterangan = htmlspecialchars($_POST["keterangan"]);
+
+    if (cekKosong($posisi) == true){
+        if (cekKosong($jobdesk) == true){
+            if (cekKosong($keterangan) == true){
+                mysqli_query($db, "UPDATE loker SET posisi = '$posisi', lulusan = '$lulusan', jobdesk = '$jobdesk', keterangan = '$keterangan' where id = $id");
+                if (mysqli_affected_rows($db) > 0){
+                    echo "
+                        <script>
+                            Swal.fire('Perubahan Berhasil Disimpan','','success').then(function(){
+                                window.location = 'data-loker.php';
+                            });
+                        </script>
+                    ";
+                }
+            }
+        }
+    }
+}
 
 ?>
